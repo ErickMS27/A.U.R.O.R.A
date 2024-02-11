c4e69c05d5b8a103f09f4414cd1ffd1c97fa8f7a
ErickMS27 <erickbackend@gmail.com>
2024-02-11 11:14:09 -0300

    Update Base Code

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,25 +1,13 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-# Criar o Chatbot
-chatbot = ChatBot('MeuBot')
-
-# Criar um novo treinador para o Chatbot
+chatbot = ChatBot('AuroraBot')
 trainer = ChatterBotCorpusTrainer(chatbot)
-
-# Treinar o Chatbot com o corpus em inglês
 trainer.train('chatterbot.corpus.english')
 
-# Loop para a interação com o Usuário
 while True:
-    user_input = input("Você: ")
-
-    # Saia do loop se o Usuário digitar 'sair'
-    if user_input.lower() == 'sair':
+    user_input = input("You: ")
+    if user_input.lower() == 'leave':
         break
-
-    # Obtenha a resposta do Chatbot
     response = chatbot.get_response(user_input)
-
-    # Imprima a resposta do Chatbot
     print("Bot:", response)
