3931175ecdd2f6d5758d689759c803deae36cce8
ErickMS27 <erickbackend@gmail.com>
2024-04-25 12:19:00 -0300

    Update chatbot code

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,13 +1,32 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import nltk
+from nltk.chat.util import Chat, reflections
 
-chatbot = ChatBot('AuroraBot')
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
+answers = [
+    [
+        
+        r"(.*)",
+        ["Desculpe, eu não entendi.", "Poderia reformular a pergunta?", 
+         "Eu não tenho certeza do que você está falando.", "Não consegui compreender, poderia repetir a pergunta?"]
+    ]
+]
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'leave':
-        break
-    response = chatbot.get_response(user_input)
-    print("Bot:", response)
+chatbot = Chat(answers, reflections)
+
+def start_chat():
+    print("\nOlá! Me chamo Aurora! Sou uma I.A gerada para te ajudar!")
+    print("Como posso ajudar você hoje?")
+    while True:
+        input_data = input("\nVocê: ")  
+        answer = chatbot.respond(input_data)  
+        print("A.U.R.O.R.A: " + answer) 
+        if input_data.lower() == "adeus":
+            print("Até mais!") 
+            break
+        elif input_data.lower() == "reiniciar":
+            print("Reiniciando o chat...")
+            start_chat()
+            break
+
+if __name__ == "__main__":
+    nltk.download("punkt", quiet=True)
+    start_chat()
